cb4ffdfe7ce1fbae966da161ba69ba7a58c56cfd
TeknoRapture <teknorapture@gmail.com>
2016-04-22 18:18:33 -0400

    Refactoring 2

---
+++ journaltestmod/JournalTest/src/tk/teknorapture/wurmunlimited/clientmods/journal/Journal.java
@@ -0,0 +1,20 @@
+/**
+ * Client side Journal Mod
+ */
+package tk.teknorapture.wurmunlimited.clientmods.journal;
+
+/**
+ * @author Teknorapture
+ * @email teknorapture@gmail.com
+ */
+
+
+public class Journal {
+
+	public Journal()
+	{
+		super();
+		
+	}
+	
+}
